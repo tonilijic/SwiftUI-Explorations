4e69a67d72fe62e2dfd699d760ba2feff1661215
Toni Lijic <hello.tonilijic@gmail.com>
2024-02-14 12:32:38 +0100

    meatball

---
+++ Meatball/Meatball/MeatballApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeatballApp.swift
+//  Meatball
+//
+//  Created by Toni on 14.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct MeatballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
