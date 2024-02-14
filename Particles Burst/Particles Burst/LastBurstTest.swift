7dbe4f24778e47b42135c2aef8a71d98f1892902
Toni Lijic <hello.tonilijic@gmail.com>
2024-02-14 13:16:01 +0100

    particles

---
+++ Particles Burst/Particles Burst/LastBurstTest.swift
@@ -0,0 +1,17 @@
+//
+//  LastBurstTestApp.swift
+//  LastBurstTest
+//
+//  Created by Toni on 08.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct LastBurstTest: App {
+    var body: some Scene {
+        WindowGroup {
+            SceneView()
+        }
+    }
+}
